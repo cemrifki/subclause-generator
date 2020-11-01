25f0c8a872d218f713fe2e4297a3b41878c4477b
cemrifki <cemrifkiaydin@gmail.com>
2020-11-01 21:41:17 +0300

    First commit.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Subclause Generation
+This repo contains the source code for generating subclauses from a given sentence in English. This functionality is also used in the work stated below. However, the following performs a comprehensive study on sentiment analysis; the creation of subclauses in a novel way is only a part of the whole work:
+
+[Combination of Recursive and Recurrent Neural Networks for Aspect-Based Sentiment Analysis Using Inter-Aspect Relations](https://ieeexplore.ieee.org/document/9078752).
+Cem Rifki Aydin, Tunga Gungor. IEEE Access
+
+This approach generates subclauses from texts on a sentence-basis in English. However, this functionality can be enhanced by taking into account several other dependency relationships. This can be adapted to other languages as well with minor changes. This also performs a basic preprocessing operation prior to extracting subclauses, which can be skipped. In addition, at the end of each subclause, we add the punctuation mark that appears at the end of the whole sentence. Lastly, redundantly appearing tokens at the beginning and end of subclauses (e.g. "and") are removed.
+
+## Requirements
+
+- Python 3.7 or a newer version of Python
+- spacy 2.1.0
+
+## Execution
+
+A simple usage of the code is given below. You can also import this module in your projects as well by performing minor changes:
+```
+from subclauses import Subclauses
+SC = Subclauses()
+subcls = SC.convert_to_subclauses("The vibe is relaxed and cozy, the service was great, and the ambiance was good!")
+print(subcls)
+# The result is: [['the', 'service', 'was', 'great', '!'], ['the', 'vibe', 'is', 'relaxed', 'and', 'cozy', '!'], ['the', 'ambiance', 'was', 'good', '!']]
+```
+
+## Citation
+If you find this code useful, please cite the following in your work:
+```
+@ARTICLE{9078752,  
+    author={C. R. {Aydin} and T. {Güngör}},  
+    journal={IEEE Access},   
+    title={Combination of Recursive and Recurrent Neural Networks for Aspect-Based Sentiment Analysis Using Inter-Aspect Relations},   
+    year={2020},  
+    volume={8},  
+    pages={77820-77832},  
+    doi={10.1109/ACCESS.2020.2990306}}}
+```
+## Credits
+Codes were written by Cem Rifki Aydin
---
+++ subclauses.py
@@ -0,0 +1,247 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+This module is used to generate subclauses from given sentences for English.
+
+@author: Cem Rıfkı Aydın
+"""
+
+import re
+from collections import defaultdict
+
+import spacy
+
+import preprocessing
+
+# Punctuation marks across the module can be handled more efficiently and consistently in future.
+p = re.compile(r"([.?!])[\"\']*$")
+
+nlp = spacy.load("en_core_web_sm")
+
+conj_and_punc_list = ["and", "or", "but", "however", "also", "?", "!", ".", ",", ":", ";", ]
+
+
+class Subclauses:
+    """
+    This class is used to generate subclauses from texts.
+    """
+
+    # The below basic set can be expanded with other relationships that you think mark the beginning of the
+    # existence of subclauses with respect to the dependency tree of the input.
+    subclause_marker_relationships = {"conj", "ccomp"}
+
+    def __init__(self):
+        pass
+
+    def convert_to_subclauses(self, sentence):
+        """
+        The helper method that converts a sentence as a whole in string format to its corresponding subclauses.
+        This actually is not used in this project, but can be employed by anyone who wants
+            to break a sentence in string format into its subclauses as list.
+
+        :param sentence: An input string: sentence to be broken down into its subclauses.
+        :type sentence: str
+        :return: The result subclauses from this sentence.
+        :rtype: list of lists
+        """
+
+        # A basic tokenizer is used to perform preprocessing operations. This could be skipped as well.
+        # Instead, you can use your own tokenizer provided that the output is of type str.
+        sentence = preprocessing.normalize_tokenize(sentence)
+
+        str_sentence_arr = [tok.text for tok in nlp(sentence)]
+
+        str_sentence = " ".join(str_sentence_arr)
+        final_punc_mark = self.final_punc_mark(str_sentence)
+
+        str_sentence = re.sub(r"[ ]+", r" ", str_sentence)
+        rev_subclauses = self.get_all_subclauses_of_sent(str_sentence)
+
+        subclauses_with_puncs = []
+
+        # The below block is mainly used to handle the final punctuation mark.
+        for ind, rev_subclause in enumerate(rev_subclauses):
+            adapted_subcl = self.remove_trailing_puncs(" ".join(rev_subclause))
+            # The final punctuation mark of the last sentence is appended to the end of all
+            # sentences. This could be updated and enhanced.
+            adapted_subcl += " " + final_punc_mark
+
+            updated_sentence = adapted_subcl.strip().split()
+            subclauses_with_puncs.append(updated_sentence)
+        return subclauses_with_puncs
+
+    def final_punc_mark(self, s):
+        """
+        This method is used to detect the final punctuation mark at the end of the text.
+
+        :param s: Text string.
+        :type s: str
+        :return: Punctuation mark at the end of the text.
+        :rtype: str
+        """
+
+        res = re.search(p, s)
+        final_punc = res.group(1) if res else "."
+        return final_punc
+
+    def get_children_recurs(self, tok, lev):
+        """
+        This method finds the children nodes of a target word recursively, all of which
+            on a whole constitute a subclause.
+        If the dependency relations stated in the subclause_marker_relationships set are encountered,
+            it marks the existence of a new subclause and therefore the recursively scanning mechanism
+            is stopped. However, this rule set could be expanded with other dependency relationships.
+
+        :param tok: The target token whose children in the subclause are to be generated.
+        :type tok: spacy.tokens.token.Token
+        :param lev: The height (level) of the recursive tree.
+        :type lev: int
+        :return: All the kids of a token in a recursive subclause tree.
+        :rtype: set
+        """
+
+        all_children = set([])
+        if lev == 0:
+            all_children.add(tok.text + "-" + str(tok.i))
+        lev += 1
+        for kid in tok.children:
+            tag = kid.tag_.lower()
+            dep = kid.dep_.lower()
+            # The below if statement segments the text into subclauses if the parser encounters a
+            # subclause-related dependency relationship and a verb marking its existence.
+            if (dep in self.subclause_marker_relationships) and (tag == "vbd" or tag == "vbz" or tag == "vbg"):
+                continue
+            all_children.add(kid.text + "-" + str(kid.i))
+            all_children |= self.get_children_recurs(kid, lev)
+        return all_children
+
+    def get_all_deps(self, sentence):
+        """
+        This helper method generates all the tokens which are connected to each other
+        via a dependency relationship.
+
+        :param sentence: The input text (e.g. sentence).
+        :type sentence: str
+        :return: All the words connected to each other via dependency relationships in the
+            same subclause.
+        :rtype: list
+        """
+
+        doc = nlp(sentence)
+        res = [self.get_children_recurs(token, 0) for token in doc]
+        return res
+
+    def get_all_subclauses_of_sent(self, sentence):
+        """
+        This helper method generates the set of all subclauses from a given sentence.
+
+        :param sentence: The input text (e.g. sentence).
+        :type sentence: str
+        :return: All the subclauses generated from "sentence".
+        :rtype: list
+        """
+
+        sent = sentence.strip()
+        final_punc = sent[-1]
+        if not re.match(r"[?.!]+$", final_punc):
+            final_punc = "."
+        subclauses = defaultdict(set)
+        all_deps = self.get_all_deps(sent)
+        all_toks = set({})
+        for dep in all_deps:
+            for tok in dep:
+                all_toks.add(tok)
+                if len(subclauses[tok]) < len(dep):
+                    subclauses[tok] = dep
+        final_subclauses = set([tuple(subclauses[tok]) for tok in all_toks])
+
+        final_subclauses_tmp = set({})
+        for ind, subcl in enumerate(final_subclauses):
+            subcl = list(subcl)
+            # Words in a subclause become ordered thanks to the below code.
+            subcl.sort(key=lambda token: int(token[token.rindex("-") + 1:]))
+            final_subclauses_tmp.add(tuple(subcl))
+
+        # Subclauses are ordered thanks to the below two lines of code.
+        final_subclauses_tmp = list(final_subclauses_tmp)
+        final_subclauses_tmp.sort(key=lambda token: int(token[0][token[0].rindex("-") + 1:]))
+
+        final_subclauses_upd = []
+
+        for subcl in final_subclauses_tmp:
+            subcl = list(subcl)
+            subcl = self.remove_ind(subcl)
+            if subcl[-1] not in ".?!;:":
+                subcl = subcl + [final_punc]
+            subcl = self.remove_trailing_conjs_and_puncs(subcl, conj_and_punc_list)
+            final_subclauses_upd.append(subcl)
+        return final_subclauses_upd
+
+    def remove_ind(self, s):
+        """
+        This is a helper method, which removes the hyphen and index information
+        from the end of a token. This is a supplementary function.
+
+        :param s: A list of words (i.e. sentence or a whole review).
+        :type s: list
+        :return: Words stripped of their hyphen and rudimentary suffixes.
+        :rtype: list
+        """
+
+        return [re.sub(re.compile(r"(.*)-[0-9]+"), r"\1", tok) for tok in s]
+
+    def remove_trailing_puncs(self, s):
+        """
+        A helper method that removes the trailing punctuation marks.
+
+        :param s: A text input.
+        :type s: str
+        :return: The text stripped off its unnecessary punctuation marks.
+        :rtype: str
+        """
+
+        return re.sub(r"[,;:.?!\"]+$", r"", s)
+
+    def remove_trailing_conjs_and_puncs(self, s, conjunction_list):
+        """
+        This method removes the trailing conjunctions and punctuation marks that are specified
+        in the variable conj_and_punc_list.
+        This is useful, since a subclause cannot in general start with a punctuation mark or, say "and".
+
+
+        :param s: A list of tokens (e.g. a sentence).
+        :type s: list
+        :param conjunction_list: The list of conjunctions and punctuation marks.
+        :type conjunction_list: list
+        :return: The sentence or subclause stripped off its unnecessary conjunctions or
+            punctuation marks in the beginning or end.
+        :rtype: list
+        """
+
+        if len(s) <= 1:
+            return s
+        while True:
+            neither_conj = 0
+            if s[0] in conjunction_list:
+                s = s[1:]
+                neither_conj += 1
+            if s[-1] in conjunction_list:
+                s = s[:-1]
+                neither_conj += 2
+            if neither_conj == 0 or len(s) <= 1:
+                break
+        return s
+
+
+if __name__ == "__main__":
+    sc = Subclauses()
+
+    # The below is an example that converts a sentence into the subclauses thereof.
+    print(sc.convert_to_subclauses("If you are not going to play this song at my funeral, then I won't be attending!"))
+
+    # Other example usages are given below.
+    print(sc.convert_to_subclauses("The service was awesome, and the food was incredible!"))
+    print(sc.convert_to_subclauses("The vibe is relaxed and cozy, the service was great, and the ambiance was good!"))
+    print(sc.convert_to_subclauses("I loved that too much!"))
+    print(sc.convert_to_subclauses("And she said: \"God, I loved this movie so much!\""))
+    print(sc.convert_to_subclauses("I just moved to this country and city, and I disliked here to be honest :(((((."))
