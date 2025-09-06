499e95fb1674a33b1c2db3d2cc47be61edd60597
cemrifki <cemrifkiaydin@gmail.com>
2025-09-06 23:38:49 +0300

    Additional packages have been added to support Turkish functionality, and the setup.py file has been updated accordingly.

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("requirements.txt") as f:
 
 setup(
     name="subclause_generator",
-    version="0.1.0",
-    description="A tool to generate subclauses using dependency parsing methods / rules.",
-    author="Cem Rıfkı Aydın",
+    version="0.2.0",
+    description="A tool to generate subclauses using dependency parsing methods / rules for both English and Turkish.",
+    author="Cem Rifki Aydin",
     packages= ["subclause_generator"],  # find_packages(),  
     install_requires=requirements,
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     include_package_data=True,
 )
 
